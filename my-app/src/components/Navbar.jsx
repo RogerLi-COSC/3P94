f10ac820384ba1b522ee91c4afc34830d7717412
RogerLi-COSC <rl20wa@brocku.ca>
2025-03-20 13:29:36 -0400

    Work now
    So far

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -8,16 +8,20 @@ export default function Navbar() {
   return (
     <nav className="navbar">
       <div className="nav-container">
-        <Link to="/" className="nav-logo">
-          Luxury Estates
-        </Link>
+        <Link to="/" className="nav-logo">Luxury Estates</Link>
+        
+        {/* Hamburger Menu Button */}
         <button className="menu-toggle" onClick={() => setIsMenuOpen(!isMenuOpen)}>
           {isMenuOpen ? "✖" : "☰"}
         </button>
-        <ul className={isMenuOpen ? "nav-menu open" : "nav-menu"}>
-          <li>
-            <Link to="/" className="nav-link">Home</Link>
-          </li>
+
+        {/* Navigation Links */}
+        <ul className={`nav-menu ${isMenuOpen ? "open" : ""}`}>
+          <li><Link to="/" className="nav-link" onClick={() => setIsMenuOpen(false)}>Home</Link></li>
+          <li><Link to="/search" className="nav-link" onClick={() => setIsMenuOpen(false)}>Search</Link></li>
+          <li><Link to="/agents" className="nav-link" onClick={() => setIsMenuOpen(false)}>Agents</Link></li>
+          <li><Link to="/messages" className="nav-link" onClick={() => setIsMenuOpen(false)}>Messages</Link></li>
+          <li><Link to="/profile" className="nav-link" onClick={() => setIsMenuOpen(false)}>Profile</Link></li>
         </ul>
       </div>
     </nav>
